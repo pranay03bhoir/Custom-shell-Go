c5d3ce1083fe0d81c92806b9d1247b26ff01432d
pranaybhoir <pranaytb777@gmail.com>
2026-05-14 18:40:13 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -33,9 +33,8 @@ func main() {
 		case strings.HasPrefix(command, "echo "): // this code is a implementation fo the echo command.
 			if strings.Contains(command[5:], "'") {
 				// cleanStr := strings.Trim(command[5:], "'")
-				cleanStr := strings.Fields(command[5:])
-				newStr := strings.ReplaceAll(strings.Join(cleanStr, " "), "'", "")
-				fmt.Println(newStr)
+				cleanStr := strings.ReplaceAll(command[5:], "'", "")
+				fmt.Println(cleanStr)
 			} else {
 				extractedWords := strings.Fields(command[5:])
 				// newStr := strings.Join(cleanStr, "")
